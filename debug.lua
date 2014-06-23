bb5c8b4a966314e453424b9e79ae79346264d476
NT <afr1ka@ubuntu.(none)>
2014-06-22 23:11:36 -0700

    Initial commit

---
+++ debug.lua
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+function read_query( packet )
+    if packet:byte() == proxy.COM_QUERY then
+        local tk = require('proxy.tokenizer')
+        local tokens = tk.tokenize(packet:sub(2))
+        local tok =1
+        print("--------------------------------\n")
+        print("num_tokens "..#tokens .. "\n")
+        print("Query: "..packet:sub(2).."\n")
+        while tok <= #tokens do
+        print(tokens[tok]['token_name'].." : "..tokens[tok]['text'].."\n")
+        tok = tok+1
+        end
+    end
+end
+
---
+++ test.lua
@@ -0,0 +1,153 @@
+function init_sec_labels ()
+
+local fr = io.open("users.txt","r")
+
+for line in fr:lines() do
+    for k, v in string.gmatch(line,"(%w+):(%d+)") do
+            proxy.global.u[k]=tonumber(v)
+    end
+end
+
+fr:close()
+
+local fre= io.open("ent.txt","r")
+
+for line in fre:lines() do
+    for t,n, v in string.gmatch(line,"(%w+):(%w+):(%d+)") do
+        if t == "DB" then
+            proxy.global.db[n]=tonumber(v)
+        end
+        if t == "T" then
+            proxy.global.t[n]=tonumber(v)
+        end
+    end
+end
+fre:close()
+
+
+end
+
+function set_error(errmsg)
+    proxy.response = {
+    type = proxy.MYSQLD_PACKET_ERR,
+    errmsg = errmsg or "error"    
+    }
+end
+
+function connect_server()
+
+if not proxy.global.u then
+    proxy.global.u={}
+    proxy.global.db={}
+    proxy.global.t={}
+    init_sec_labels()
+    end
+
+end
+
+function user_sec_label()
+
+for k,v in pairs(proxy.global.u) do
+        if k == proxy.connection.client.username then
+            return v
+            end
+    end
+
+return -1
+end
+
+function ent_sec_label(ent_n)
+        for k,v in pairs(proxy.global.t) do
+           if k == ent_n then
+               return v
+           end
+        end
+     for k,v in pairs(proxy.global.db) do
+         if k == ent_n then
+             return v
+         end
+     end
+
+return -1
+end
+
+
+function sel_check_access(tokens,tok)
+
+while tok <= #tokens do
+
+    if tokens[tok]['token_name'] ~= "TK_FUNCTION"  then
+        if tokens[tok]['token_name'] == "TK_SQL_FROM" then
+            while tok <= #tokens do
+                tok = tok +1
+                if tokens[tok]['token_name'] == 'TK_SQL_AS' then
+                    tok=tok+1
+                elseif tokens[tok]['token_name'] == 'TK_SQL_LITERAL' then
+
+
+                    end
+            --ul = user_sec_label()
+            --el = ent_sec_label(tokens[tok+1]['text'])
+            --print("Ent_l "..tokens[tok+1]['text'].."\n")
+            --print("Lables u="..ul.." e="..el.."\n")
+            if ul >= el then
+                return tok+1,false
+            else 
+                return tok+1,true
+            end
+            end
+        end
+    else 
+        return tok,false
+    end
+tok = tok +1
+
+end
+
+return tok,false
+
+end
+
+function ins_check_access(tokens,tok)
+
+end
+
+function upd_check_access(tokens,tok)
+
+end
+
+function del_check_access(tokens,tok)
+
+end
+
+
+function read_query( packet )
+	if packet:byte() == proxy.COM_QUERY then
+        local tk = require('proxy.tokenizer')
+        local tokens = tk.tokenize(packet:sub(2))
+        local tok =1
+        print("num_tokens "..#tokens .. "\n")
+        print("Query: "..packet:sub(2).."\n")
+        while tok <= #tokens do
+            print(tok)
+        res = false
+             if tokens[tok]['token_name'] == "TK_SQL_SELECT" then
+                 tok,res = sel_check_access(tokens,tok)
+             elseif res ==false and tok ~= #tokens and  tokens[tok]['token_name'] == "TK_SQL_INSERT" then
+                 tok,res = ins_check_access(tokens,tok)
+             elseif res == false and tok ~= #tokens and  tokens[tok]['token_name'] == "TK_SQL_UPDATE" then
+                 tok,res = upd_check_access(tokens,tok)
+             elseif res == false and tok ~= #tokens and  tokens[tok]['token_name'] == "TK_SQL_DELETE" then
+                 tok,res = del_check_access()
+             end
+    
+             if res == true then
+                  set_error("Query ("..packet:sub(2)..") was blocked")
+                  return proxy.PROXY_SEND_RESULT
+             end
+        tok = tok+1
+        end
+    end
+end
+
+
