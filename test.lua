826d64a4814b93bd4439e7abc4389a88f7e7bac6
root <root@localhost.localdomain>
2014-08-10 03:49:06 +0700

    Complex insert and update methods are supported.

--- test.lua
+++ test.lua
@@ -409,15 +409,9 @@ while tok <= max_tokens do
 
 
             ul = user_sec_label()
-            --el = ent_sec_label(tokens[tok]['text'],true)
             print("Ent_l "..tokens[tok]['text'].."\n")
             print("Lables u="..ul.." e="..el.."\n")
             
-            --if ul >= el then
-                --return tok+1,false
-            --else
-                --return tok+1,true
-            --end
             return tok+1,access_write(ul,el,false)    
         else
             return tok,true
@@ -485,7 +479,6 @@ function read_query( packet )
 	if packet:byte() == proxy.COM_QUERY then
         local tk = require('proxy.tokenizer')
         local tokens = tk.tokenize(packet:sub(2))
-        --local tokens=tk.tokens_without_comments(row_tokens)
         local parse = require('proxy.parser')
         local tok =1
         print("num_tokens "..#tokens .. "\n")
@@ -535,6 +528,61 @@ function read_query( packet )
             end
 
         elseif tokens[tok]['token_name'] == "TK_SQL_INSERT" or tokens[tok]['token_name'] == "TK_SQL_UPDATE" then
+            local sq=sub_query_tokenize(tokens)
+            local w_obj_l = 0
+            local ul=user_sec_label()
+            local count = 0
+            for t=1,#sq do
+                count=count +1
+                local tbls = parse.get_tables(sq[t])
+                print('Tables in query = '..#tbls)
+                for k,v in pairs(tbls) do
+                    local db,tab = k:match("([^.]+).([^.]+)")
+                    local clms = find_columns(sq[t],db,tab)
+                    local find = false
+                        for c,fl in pairs(clms) do
+                            print('Column name = '..c)
+                            if fl==true then
+                                find=true
+                                local el=ent_sec_label(true,2,db,tab,c)
+                                if #sq>1 and w_obj_l==0 and count == 1 then
+                                    print("User_label = "..ul.."\nEnt_label = "..el.."\nDB = "..db.." Table = "..tab)
+                                    res=access_append(ul,el,false)
+                                    w_obj_l=el
+                                elseif #sq == 1 then
+                                    res=access_append(ul,el,false)
+                                elseif #sq>1 and count>1 then
+                                    res=access_append(ul,w_obj_l,true,el)
+                                end
+                                if res == true then
+                                    set_error("Query ("..packet:sub(2)..") was blocked")
+                                    return proxy.PROXY_SEND_RESULT
+                                end
+                                print(res)
+                            end
+                        end 
+                    if find==false then 
+                        local el=ent_sec_label(false,1,db,tab)
+                        if #sq>1 and w_obj_l==0 and count == 1 then
+                            print("No columns. User_label = "..ul.."\nEnt_label = "..el.."\nDB = "..db.." Table = "..tab)
+                            res = access_append(ul,el,false)
+                            w_obj_l = el
+                        elseif #sq == 1 then
+                            print('Sq==1')
+                            res = access_append(ul,el,false)
+                        elseif #sq>1 and count>1 then
+                            print('El == '..el)
+                            res = access_append(ul,w_obj_l,true,el)
+                        end
+
+                        if res == true then
+                            set_error("Query ("..packet:sub(2)..") was blocked")
+                            return proxy.PROXY_SEND_RESULT
+                        end
+                    end
+                    
+                end
+            end
         end
 
         if res == true then
