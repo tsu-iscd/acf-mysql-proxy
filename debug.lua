7e5cd2e02dc31da625201cc22b970f9eaa5c8ee5
NT <afr1ka@ubuntu.(none)>
2014-08-09 03:17:16 -0700

    Normalizatin is added (commented).

--- debug.lua
+++ debug.lua
@@ -9,19 +9,22 @@ function read_query( packet )
     if packet:byte() == proxy.COM_QUERY then
         local parse = require('proxy.parser')
         local tk = require('proxy.tokenizer')
-        local tokens = tk.tokenize(packet:sub(2))
+        local tokens  = tk.tokenize(packet:sub(2))
+        --local toks = tk.normalize(tokens)
         local tok =1
         print("--------------------------------\n")
         print("DB: "..proxy.connection.client.default_db.."\n")
         print("num_tokens "..#tokens .. "\n")
         print("Query: "..packet:sub(2).."\n")
+
         tbls = parse.get_tables(tokens)
         for k,v in pairs(tbls) do
             print('tables: '..k..'\nsql: '..v..'\n')
         end
+        --print("Toks: "..toks.."\n")
         while tok <= #tokens do
-        print(tokens[tok]['token_name'].." : "..tokens[tok]['text'].."\n")
-        tok = tok+1
+            print(tokens[tok]['token_name'].." : "..tokens[tok]['text'].."\n")
+            tok = tok+1
         end
     end
 end
